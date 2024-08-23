5b1117f26940c0fa06e61628ca9963654c96d1be
SerJv21 <serjv@live.ru>
2024-08-23 16:14:14 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,19 @@
+"1st program"
+print(9 **0.5 *5)
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 +2))
+print(6 == 8)
+"4th program"
+# Дана стррока '123.456' - нужно вывести на экран цифру после запятой это 4
+print(float('123.456'))
+print(123.456 * 10)
+print(int(1234.56 % 10))
+
+print(type('123.456'))
+print(type(float('123.456')))
+print(float('123.456'))
+print(123.456 * 10)
+print(int(1234.56 % 10))
